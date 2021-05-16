e0600599aa7b397d9ebf13a62e2faf9d8cb6a628
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-05-15 23:25:00 -0400

    implement POST and README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # python-vault-api-cli
-python-vault-api-cli
+![Vault Image](https://d1q6f0aelx0por.cloudfront.net/product-logos/library-vault-logo.png)
+
+## Summary
+Vault http api wrapper in python.
+
+## Usage
+CLI menu driven currently supports GET and POST methods.
+* Input vault_addr variable
+* Input vault_token variable
+* Profit?
+
+## Logging
+Each request is logged into an output.log file with a timestamp in the current working directory. 
+
+## GET
+Work is being done on adding more GET endpoints.
+
+## POST
+POST has just been implemented with /v1/sys/capbilities - e.g., lookup a token's capabilities.
--- vault.py
+++ vault.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import requests
+import datetime
 import time
 import sys
 from simple_term_menu import TerminalMenu
@@ -8,16 +10,21 @@ from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
+log_name = 'output.log'
 vault_addr = ''
 vault_headers = {'X-Vault-Token' : ''}
-api_endpoints = [
+api_endpoints_get = [
   '/v1/sys/audit',
   '/v1/sys/auth'
   ]
 
+api_endpoints_post = [
+  '/v1/sys/capabilities'
+  ]
+
 def main():
     main_menu_title = " Main Menu\n"
-    main_menu_items = ["vault_sys_audit", "vault_sys_auth", "quit"]
+    main_menu_items = ["vault_sys_audit", "vault_sys_auth", "sys_capabilities", "quit"]
     main_menu_cursor = "> "
     main_menu_cursor_style = ("fg_red", "bold")
     main_menu_style = ("bg_green", "fg_black")
@@ -50,20 +57,29 @@ def main():
         main_sel = main_menu.show()
         if main_sel == 0:
             # call audit endpoint
-            vault_api_call(api_endpoints[0])
+            vault_api_call_get(api_endpoints_get[0])
         elif main_sel == 1:
             # call auth endpoint
-            vault_api_call(api_endpoints[1])
+            vault_api_call_get(api_endpoints_get[1])
         elif main_sel == 2:
+            # call sys capabilities
+            token = input('Token: ')
+            paths_input = input('Paths: ')
+            paths = paths_input.split()
+            post_args = {"token": token, "paths": paths}
+            vault_api_call_post(api_endpoints_post[0],post_args)
+        elif main_sel == 3:
             main_menu_exit = True
-        if (main_sel >= 0) and (main_sel < 2):
+        if (main_sel >= 0) and (main_sel < 3):
             back_menu.show()
 
-def vault_api_call(api_endpoint):
+def vault_api_call_get(api_endpoint):
   try:
     response = requests.get(vault_addr+api_endpoint, headers=vault_headers, verify=False)
     response.raise_for_status()
     print(response.json())
+    r = str(response.json())
+    log_file(log_name,r)
   except requests.exceptions.HTTPError as errh:
     print(errh)
   except requests.exceptions.ConnectionError as errc:
@@ -72,6 +88,32 @@ def vault_api_call(api_endpoint):
     print(errt)
   except requests.exceptions.RequestException as err:
     print(err)
+
+def vault_api_call_post(api_endpoint, post_args):
+  try:
+    response = requests.post(vault_addr+api_endpoint, headers=vault_headers, data=post_args, verify=False)
+    response.raise_for_status()
+    print(response.json())
+    r = str(response.json())
+    log_file(log_name,r)
+  except requests.exceptions.HTTPError as errh:
+    print(errh)
+  except requests.exceptions.ConnectionError as errc:
+    print(errc)
+  except requests.exceptions.Timeout as errt:
+    print(errt)
+  except requests.exceptions.RequestException as err:
+    print(err)
+
+def log_file(log_name,r):
+    now = datetime.datetime.now()
+    timestamp = str(now.strftime("%Y%m%d_%H:%M:%S"))
+    try:
+      f = open(log_name+'_'+timestamp, 'a')
+      f.write(r)
+      f.close
+    except Exception as err:
+      print(str(err))
 
 if __name__ == "__main__":
     main()
