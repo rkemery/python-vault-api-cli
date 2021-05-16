77b0f11d5266adf166d79a3a33ee283b7baab5e0
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-05-16 14:44:16 -0400

    add api doc function, edited readme, and reqs

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Vault http api wrapper in python. Currently in early work phase.
 ## Usage
 CLI menu driven, which currently supports GET and POST methods.
 * Input vault_addr variable.
-* Input vault token under vault_headers variable.
+* Input vault token under vault_token variable.
 * Profit?
 
 ## Logging
@@ -18,3 +18,6 @@ Work is being done on adding more GET endpoints.
 
 ## POST
 POST has just been implemented with /v1/sys/capbilities - e.g., lookup a token's capabilities.
+
+## Updates
+Each call now generates the response body but also pulls the API endpoint summary from documentation (this is not logged).
--- vault.py
+++ vault.py
@@ -1,30 +1,55 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import requests
 import datetime
 import sys
 import json
 from simple_term_menu import TerminalMenu
 from urllib3.exceptions import InsecureRequestWarning
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 log_name = 'output.log'
 vault_addr = ''
-vault_headers = {'X-Vault-Token' : ''}
+vault_token = ''
+vault_headers = {'X-Vault-Token' : vault_token}
+
 api_endpoints_get = [
   '/v1/sys/audit',
-  '/v1/sys/auth'
+  '/v1/sys/auth',
+  '/v1/sys/health',
+  '/v1/sys/host-info',
+  '/v1/sys/internal/ui/mounts'
   ]
 
 api_endpoints_post = [
   '/v1/sys/capabilities'
   ]
 
+api_info_urls = [
+  'https://www.vaultproject.io/api-docs/system/audit',
+  'https://www.vaultproject.io/api-docs/system/auth',
+  'https://www.vaultproject.io/api-docs/system/health',
+  'https://www.vaultproject.io/api-docs/system/host-info',
+  'https://www.vaultproject.io/api-docs/system/internal-ui-mounts',
+  'https://www.vaultproject.io/api-docs/system/capabilities'
+  ]
+
 def main():
     main_menu_title = " Main Menu\n"
-    main_menu_items = ["vault_sys_audit", "vault_sys_auth", "sys_capabilities", "quit"]
+    main_menu_items = [
+            "vault_sys_audit", 
+            "vault_sys_auth", 
+            "vault_sys_health",
+            "vault_sys_host_info",
+            "vault_sys_internal_ui_mounts",
+            "sys_capabilities", 
+            "quit"
+            ]
     main_menu_cursor = "> "
     main_menu_cursor_style = ("fg_red", "bold")
     main_menu_style = ("bg_green", "fg_black")
@@ -56,21 +81,36 @@ def main():
     while not main_menu_exit:
         main_sel = main_menu.show()
         if main_sel == 0:
-            # call audit endpoint
+            # call sys/audit
             vault_api_call_get(api_endpoints_get[0])
+            api_info(api_info_urls[0])
         elif main_sel == 1:
-            # call auth endpoint
+            # call sys/auth
             vault_api_call_get(api_endpoints_get[1])
+            api_info(api_info_urls[1])
         elif main_sel == 2:
-            # call sys capabilities
+            # call sys/health
+            vault_api_call_get(api_endpoints_get[2])
+            api_info(api_info_urls[2])
+        elif main_sel == 3:
+            # call sys/host-info
+            vault_api_call_get(api_endpoints_get[3])
+            api_info(api_info_urls[3])
+        elif main_sel == 4:
+            # call sys/internal/ui/mounts
+            vault_api_call_get(api_endpoints_get[4])
+            api_info(api_info_urls[4])
+        elif main_sel == 5:
+            # call sys/capabilities
             token = input('Token: ')
             paths_input = input('Paths: ')
             paths = paths_input.split()
             post_args = {"token": token, "paths": paths}
             vault_api_call_post(api_endpoints_post[0],post_args)
-        elif main_sel == 3:
+            api_info(api_info_urls[5])
+        elif main_sel == 6:
             main_menu_exit = True
-        if (main_sel >= 0) and (main_sel < 3):
+        if (main_sel >= 0) and (main_sel < 6):
             back_menu.show()
 
 def vault_api_call_get(api_endpoint):
@@ -114,6 +154,17 @@ def log_file(log_name,req):
       f.close
     except Exception as err:
       print(str(err))
+
+def api_info(url):
+    try:
+      html = urlopen(url).read()
+      soup = BeautifulSoup(html, features="html.parser")
+      text = soup.find("p", {"class" : "g-type-long-body"})
+      text = str(text.contents).replace(r'\n',' ').replace('<code>','').replace('</code>','').replace("'","").replace(' , ', ' ').replace(',','',1)
+    except HTTPError as hp:
+      print(hp)
+    else:
+      print(text)
 
 if __name__ == "__main__":
     main()
